325f7b215574622d6935981541fe1d7610509b79
Shivarajkumbhar <109192668+Shivarajkumbhar@users.noreply.github.com>
2023-09-15 12:40:48 +0530

    Update switch inputcode.py

--- switch inputcode.py
+++ switch inputcode.py
@@ -4,3 +4,19 @@ btn = button(4)
 if btn.is_pressed:
   print('button is pressed')
   sleep(1)
+
+
+#led on when button preesed
+
+from gpiozero import Button
+from time import sleep
+from gpiozero import LED
+btn = Button(4)
+led = LED(2)
+led.off()
+	if btn.is_pressed:
+		print('Btn pressed')
+		led.on
+		sleep(1)
+	else:
+		led.off
